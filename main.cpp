cd1f35dd3d5c704d8ec33b94f9062649a787e288
robybert <robbert2335@gmail.com>
2024-01-24 14:16:30 +0100

    added functionality to restart processes

--- main.cpp
+++ main.cpp
@@ -5,16 +5,56 @@
 #include <thread>
 #include <unistd.h>
 #include <time.h>
+#include <sys/types.h>
+#include <signal.h>
 #include "process.cpp"
 
 // const int CHILDREN = 5;
 const int PARENT_SEED = 463523;
 using namespace std;
 
+int make_pipe(int fildes[2], int pipe_nr) {
+    int status;
+    status = pipe(fildes);
+    if (status == -1) {
+        fprintf(stderr, "failure to create pipe %d\n", pipe_nr);
+        // TODO: handle error
+        return -1;
+    }
+    return 0;
+}
+
+pid_t fork_process(int process_nr, int fildes[CHILDREN][2], bool restart) {
+    pid_t c_pid;
+    c_pid= fork();
+    if (c_pid == -1){
+        fprintf(stderr, "failure to fork child %d\n", c_pid);
+        exit(EXIT_FAILURE);
+        //TODO: handle exit failure
+    } else if (c_pid == 0) {
+        //child
+        cout << "child process created " << getpid() << endl;
+
+        //let child exit w/o making more children
+        return msg_process(process_nr,fildes, restart);
+    }
+    return c_pid;
+}
+
 
 // simulates a failure and restarts the process
-int restart_process(int process_nr, pid_t pid){
-    return 0;
+pid_t restart_process(int process_nr, pid_t pid, int fildes[CHILDREN][2]){
+    //TODO: kill the process
+    pid_t new_c_pid;
+    kill(pid, SIGKILL);
+    //TODO: make new pipes and close the old pipes
+    if (make_pipe(fildes[process_nr], process_nr) == -1) {
+        //TODO: make error
+    }
+
+    //TODO: refork the process and let it send its fd to other processes 
+    new_c_pid = fork_process(process_nr, fildes, true);
+    return new_c_pid;
 }
 
 int main(int argc, char const *argv[])
@@ -22,31 +62,16 @@ int main(int argc, char const *argv[])
     // setup RNG
     srand(PARENT_SEED);
     //establish pipes
-    int status[CHILDREN];
     int fildes[CHILDREN][2];
 
     for (int i = 0; i < CHILDREN; i++) {
-        status[i] = pipe(fildes[i]);//check if working
-        if (status[i] == -1) {
-            fprintf(stderr, "failure to create pipe %d\n", i);
-            exit(EXIT_FAILURE);
-        }
+        make_pipe(fildes[i], i);
     }
     //fork all child processes
     pid_t c_pid[CHILDREN];
 
     for (int i = 0; i < CHILDREN; i++){
-        c_pid[i] = fork();
-        if (c_pid[i] == -1){
-            fprintf(stderr, "failure to fork child %d\n", c_pid[i]);
-            exit(EXIT_FAILURE);
-        } else if (c_pid[i] == 0) {
-            //child
-            cout << "child process created " << getpid() << endl;
-
-            //let child exit w/o making more children
-            return msg_process(i,fildes);
-        }
+        c_pid[i] = fork_process(i, fildes, false);
         //parent
         cout << "parent created child " << i << " with c_pid " << c_pid[i] << endl;
 
@@ -55,11 +80,11 @@ int main(int argc, char const *argv[])
     }
     while(1) {
         //rng for time to wait 
-        this_thread::sleep_for(chrono::seconds(rand()%3));
+        this_thread::sleep_for(chrono::seconds(3));
         //rng for process selection
-        int to_restart = rand() % CHILDREN;
+        //int to_restart = rand() % CHILDREN;
         //restart the selected process
-        restart_process(to_restart,c_pid[to_restart]);
+        //c_pid[to_restart] = restart_process(to_restart, c_pid[to_restart], fildes);
         
     }
     return 0;
--- process.cpp
+++ process.cpp
@@ -11,9 +11,27 @@
 
 using namespace std;
 
-struct msg_t{
+typedef enum message_type {
+    MSG,
+    ERR
+} msg_type;
+
+struct ptp_msg {
     int sending_process_nr;
     char msg_buf[50];
+};
+
+struct ptp_err {
+    int sending_process_nr;
+    int fildes[2];
+};
+
+struct msg_t {
+    message_type msg_type;
+    union contents {
+        struct ptp_msg ptp_msg;
+        struct ptp_err ptp_err;
+    } contents;
 };
 
 const int BSIZE = sizeof(msg_t);
@@ -22,7 +40,7 @@ const int CHILDREN = 5;
 
 
 
-int msg_process(int process_nr,int fildes[CHILDREN][2])
+int msg_process(int process_nr,int fildes[CHILDREN][2], bool restart)
 {
     cout << "msg_process " << process_nr << " started with pid = " << getpid() << endl;
 
@@ -39,6 +57,25 @@ int msg_process(int process_nr,int fildes[CHILDREN][2])
     srand(SEED);
     tv.tv_sec = 2;
     tv.tv_usec = 0;
+
+    if (restart) {
+        buffer.msg_type = ERR;
+        
+        buffer.contents.ptp_err.fildes[0] = fildes[process_nr][0];
+        buffer.contents.ptp_err.fildes[1] = fildes[process_nr][1];
+        buffer.contents.ptp_err.sending_process_nr = process_nr;
+        for (int i = 0; i < CHILDREN; i++)
+        {
+            if (i == process_nr) continue;
+            ret = write(fildes[i][1], &buffer, BSIZE);
+            if (ret <0)
+            {
+                cout << process_nr << " failure to write err msg to " << i << endl;
+            }
+            
+        }
+        
+    }
     
     while(1) {
         ready_fd = current_fd;
@@ -51,14 +88,22 @@ int msg_process(int process_nr,int fildes[CHILDREN][2])
             perror("select error");
         } else if (FD_ISSET(fildes[process_nr][0], &ready_fd)){
             ret = read(fildes[process_nr][0], &buffer, BSIZE);
-            msg_cnt++;
-            cout << process_nr << " " << buffer.msg_buf << " " << msg_cnt << endl;
+            if (buffer.msg_type == MSG) {
+                msg_cnt++;
+            cout << process_nr << " " << buffer.contents.ptp_msg.msg_buf << " " << msg_cnt << endl;
             continue;
+            } else if (buffer.msg_type == ERR) {
+                //TODO: replace the fildes of the old pipe
+                fildes[buffer.contents.ptp_err.sending_process_nr][0] = buffer.contents.ptp_err.fildes[0];
+                fildes[buffer.contents.ptp_err.sending_process_nr][1] = buffer.contents.ptp_err.fildes[1];
+            }
+            
         }
         //start sending messages
         //cout << "sending message " << process_nr << endl;
-        sbuffer.sending_process_nr = process_nr;
-        sprintf(sbuffer.msg_buf, "message sent form process %d      ", process_nr);
+        sbuffer.msg_type = MSG;
+        sbuffer.contents.ptp_msg.sending_process_nr = process_nr;
+        sprintf(sbuffer.contents.ptp_msg.msg_buf, "message sent form process %d      ", process_nr);
         while ((dest_process_nr = rand() % CHILDREN) == process_nr) {}
         ret = write(fildes[dest_process_nr][1], &sbuffer, BSIZE);
         if (ret < 0) {
